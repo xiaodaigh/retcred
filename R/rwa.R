f8ccd59f136174a16fb585f83b3f30f74304802e
rstudio <rstudio@example.com>
2015-01-02 03:13:23 +0000

    minor changes for CRAN

--- R/rwa.R
+++ R/rwa.R
@@ -1,11 +1,10 @@
-#' Risk-weighted Asset (RWA) and the required capital, k, calculation for retail portfolios under the
-#' BasellII framework
+#' Risk-weighted Asset (RWA) and the required capital, k, calculation for retail
+#' portfolios under the Basel II framework
 #' 
 #' @param pd The probability of default (PD) of the record
 #' @param lgd The loss given default (LGD) of the record
 #' @param asset.class The assest class of the account
 #' @param r The assest correlation. Usually not required as it is specifeid under BaselII bassed on asset class
-#' @param reg.mult Regulator specified multiplier to the RWA
 #' @export
 #' 
 k.capital <- function(pd, lgd, asset.class = c("Mortgage", "QRRE", "Other Retail"), r = NULL) {
@@ -25,6 +24,8 @@ k.capital <- function(pd, lgd, asset.class = c("Mortgage", "QRRE", "Other Retail
 #' @rdname k.capital
 #' @param ead The exposure at default in dollar amount. If not specified the 
 #'   function returns the RWA\%
+#' @param reg.mult Regulator specified multiplier to the RWA
+#' @param ... arguments that feed into the k.capital function
 #' @examples
 #' rwa(pd = 0.01, lgd = 0.2) 
 #' 
