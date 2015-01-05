55c6bf2cd942c3799b410de6be435bbe297ff149
rstudio <rstudio@example.com>
2015-01-05 05:53:18 +0000

    updated gini code to not generate errors

--- R/gini.R
+++ R/gini.R
@@ -2,6 +2,7 @@
 #   no visible binding for global variable notes
 globalVariables(c("sum.bad","sum.all","cum.sum.bad","cum.sum","cum.sum.bad.p",'cum.sum.p',"area"))
 
+
 #' GINI coefficient for binary targets
 #' 
 #' @param score A vector of numbers that is used to predict the target.
@@ -9,25 +10,27 @@ globalVariables(c("sum.bad","sum.all","cum.sum.bad","cum.sum","cum.sum.bad.p",'c
 #' @param bad The bad target. It must be one of the two values in target.
 #' @param return.data Return the data and the intermediate computation results.
 #' @param return.data.table Return the data as data.table. By default this is FALSE because data.frame is more common.
-#' @export
+#' @import dplyr
 #' @importFrom data.table data.table
 #' @importFrom magrittr %<>%
-#' @import dplyr
+#' @export
 #' @examples
 #' # this should return a gini close to 0 since the scores are assigned randomly
 #' gini(rnorm(100), rnorm(100) < 0) 
 #' 
 gini <- function(score, target, bad = NULL, return.data = T, return.data.table = F) {  
-  browser()
+  #browser()
   # determine the bad if not already specified
   if(is.null(bad))
-    bad <- sort(unique(target))[1]  
+    bad <- sort(unique(target))[1]
 
   # use data.table to compute the score and target table
   dt <- data.table(score,target, key = "score")
   
   # computate the cumulative bads# and cumulative totals# 
-  dt <- dt %>% group_by(score) %>%     
+  dt <- dt %>% 
+    group_by(score) %>% 
+    mutate(bad = rep(bad,nrow(dt))) %>%
     summarise(sum.bad = sum(target == bad), sum.all = n()) %>% 
     mutate(cum.sum.bad = cumsum(sum.bad), cum.sum = cumsum(sum.all))
     
